a5f32b079f08a666cdf3af3fd9dcda0a6112abfc
Petr Horník <petrhornik2007@outlook.cz>
2025-01-04 21:06:59 +0100

    Notes i got so far 4.1.25

---
+++ 02_umisteni/03_externiJS/scripty.js
@@ -0,0 +1,14 @@
+/*Toto je čistě JS soubor = sem se píší pouze JS scripty či fuknce*/ 
+
+function mojeFUNKCE()
+{
+    document.getElementById("test").innerHTML = "Text psaný JS!!";
+}
+
+/*
+    Výhody externího JS souboru:  
+
+    1) Přehlednost mezi JS a HTML
+    2) jednodušší opravy chyb
+    3) JS se tímto způsobem načitá rychleji(projeví se až u delšího kódu)
+*/
---
+++ 04_statements/05_keywordy/zapisky.md
@@ -0,0 +1,3 @@
+Javascript statementy obvykle začínají keywordy pro identifikaci akce co se provede/bude provádět
+
+ Keywordy v tabulce se naučím v w3schools, ale existuje jich víc
